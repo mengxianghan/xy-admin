97d0d43d8d8490abb55ac0c69447bf7499732a9f
孟祥涵 <1056811341@qq.com>
2022-05-03 08:59:36 +0800

    fix 修复未缓存页面刷新无效问题

--- src/store/modules/multiTab.js
+++ src/store/modules/multiTab.js
@@ -52,7 +52,8 @@ const mutations = {
             state.list.push(value)
         }
         // 更新缓存列表
-        state.cacheList = state.list.filter(item => item?.meta?.keepAlive).map(item => item.name)
+        state.cacheList = state.list.filter(item => item?.meta?.keepAlive)
+                               .map(item => item.name)
     },
     /**
      * 更新缓存列表
@@ -61,16 +62,22 @@ const mutations = {
      * @constructor
      */
     UPDATE_CACHE_LIST(state, {route}) {
-        const index = findIndex(state.cacheList, o => o === route.name)
-        // 判断是移除或添加
-        if (index > -1) {
-            // 已存在，执行移除操作
-            state.cacheList.splice(index, 1)
-            state.keepAlive = false
+        // 判断是否是需要缓存的页面
+        if (route?.meta?.keepAlive) {
+            const index = findIndex(state.cacheList, o => o === route.name)
+            // 判断是移除或添加
+            if (index > -1) {
+                // 已存在，执行移除操作
+                state.cacheList.splice(index, 1)
+                state.keepAlive = false
+            } else {
+                // 不存在，执行添加操作
+                state.cacheList.push(route.name)
+                state.keepAlive = true
+            }
         } else {
-            // 不存在，执行添加操作
-            state.cacheList.push(route.name)
-            state.keepAlive = true
+            // 不是需要缓存的页面
+            state.keepAlive = !state.keepAlive
         }
         // 如果刷新的是 iframe
         if ('iframe' === route?.meta?.type) {
@@ -87,13 +94,14 @@ const mutations = {
      * @constructor
      */
     UPDATE_IFRAME_LIST(state) {
-        state.iframeList = state.list.filter(item => 'iframe' === item?.meta?.type).map(item => ({
-            path: item.path,
-            meta: {
-                type: item.meta.type,
-                url: item.meta.url,
-            },
-        }))
+        state.iframeList = state.list.filter(item => 'iframe' === item?.meta?.type)
+                                .map(item => ({
+                                    path: item.path,
+                                    meta: {
+                                        type: item.meta.type,
+                                        url: item.meta.url,
+                                    },
+                                }))
     },
 }
 
