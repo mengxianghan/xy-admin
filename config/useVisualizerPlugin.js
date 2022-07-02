a0726d4818efc94935c25df8084f6a9e65993f74
孟祥涵 <1056811341@qq.com>
2022-07-02 16:20:51 +0800

    chore

--- config/useVisualizerPlugin.js
+++ config/useVisualizerPlugin.js
@@ -1,10 +1,14 @@
 import visualizer from 'rollup-plugin-visualizer'
 
+const lifecycle = process.env.npm_lifecycle_event
+
 export default () => {
-    return visualizer({
-        filename: './node_modules/.cache/visualizer/stats.html',
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-    })
+    if ('report' === lifecycle) {
+        return visualizer({
+            filename: './node_modules/.cache/visualizer/stats.html',
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+        })
+    }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import pkg from './package.json'
 
 import useCompressPlugin from './config/useCompressPlugin'
 import useProgressPlugin from './config/useProgressPlugin'
 import useVuePlugin from './config/useVuePlugin'
+import useVisualizerPlugin from './config/useVisualizerPlugin'
 import useServer from './config/useServer'
-
-import pkg from './package.json'
 
 const { VITE_PUBLIC_PATH } = process.env
 
@@ -17,6 +17,14 @@ export default defineConfig({
         cssTarget: 'chrome80',
         brotliSize: false,
         chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    tinymce: ['tinymce'],
+                    echarts: ['echarts'],
+                },
+            },
+        },
     },
     css: {
         preprocessorOptions: {
@@ -38,7 +46,7 @@ export default defineConfig({
             version: pkg.version,
         }),
     },
-    plugins: [useVuePlugin(), useProgressPlugin(), useCompressPlugin()],
+    plugins: [useVuePlugin(), useProgressPlugin(), useCompressPlugin(), useVisualizerPlugin()],
     server: useServer(),
     resolve: {
         alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
