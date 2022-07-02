0937ddeec0da07ac27d4a4ec70e9f8bda0f4922d
孟祥涵 <1056811341@qq.com>
2022-07-02 16:51:51 +0800

    fix: 修复环境变量文件读取错误问题

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import pkg from './package.json'
 
@@ -8,47 +8,49 @@ import useVuePlugin from './config/useVuePlugin'
 import useVisualizerPlugin from './config/useVisualizerPlugin'
 import useServer from './config/useServer'
 
-const { VITE_PUBLIC_PATH } = process.env
-
-export default defineConfig({
-    build: {
-        base: VITE_PUBLIC_PATH,
-        target: 'es2015',
-        cssTarget: 'chrome80',
-        brotliSize: false,
-        chunkSizeWarningLimit: 2000,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    tinymce: ['tinymce'],
-                    echarts: ['echarts'],
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    return defineConfig({
+        build: {
+            base: env.VITE_PUBLIC_PATH,
+            outDir: env.VITE_OUT_DIR,
+            target: 'es2015',
+            cssTarget: 'chrome80',
+            brotliSize: false,
+            chunkSizeWarningLimit: 2000,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        tinymce: ['tinymce'],
+                        echarts: ['echarts'],
+                    },
                 },
             },
         },
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                modifyVars: {
-                    hack: `
-                        true; 
-                        @import '${path.resolve(__dirname, 'src/styles/vars.less')}';
-                        @import '${path.resolve(__dirname, 'src/styles/util.less')}';
-                     `,
+        css: {
+            preprocessorOptions: {
+                less: {
+                    modifyVars: {
+                        hack: `
+                            true; 
+                            @import '${path.resolve(__dirname, 'src/styles/vars.less')}';
+                            @import '${path.resolve(__dirname, 'src/styles/util.less')}';
+                         `,
+                    },
+                    javascriptEnabled: true,
                 },
-                javascriptEnabled: true,
             },
+            devSourcemap: true,
         },
-        devSourcemap: true,
-    },
-    define: {
-        __APP_INFO__: JSON.stringify({
-            version: pkg.version,
-        }),
-    },
-    plugins: [useVuePlugin(), useProgressPlugin(), useCompressPlugin(), useVisualizerPlugin()],
-    server: useServer(),
-    resolve: {
-        alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-    },
-})
+        define: {
+            __APP_INFO__: JSON.stringify({
+                version: pkg.version,
+            }),
+        },
+        plugins: [useVuePlugin(), useProgressPlugin(), useCompressPlugin(), useVisualizerPlugin()],
+        server: useServer(),
+        resolve: {
+            alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+        },
+    })
+}
