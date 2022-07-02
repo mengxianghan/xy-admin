7a42e112f4d804caefb399222afc132c9f13a185
孟祥涵 <1056811341@qq.com>
2022-07-02 15:16:50 +0800

    refactor: 重构二维码组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,54 +1,54 @@
-import ActionBar from "./ActionBar.vue";
-import ActionButton from "./ActionButton.vue";
-import Breadcrumb from "./Breadcrumb.vue";
-import Chart from "./Chart.vue";
-import Cropper from "./Cropper.vue";
-import CropperModal from "./CropperModal.vue";
-import Editor from "./Editor.vue";
-import FormTable from "./FormTable.vue";
-import Filter from "./Filter/Filter.vue";
-import FilterItem from "./Filter/FilterItem.vue";
-import LayoutHeader from "./LayoutHeader.vue";
-import LayoutMenu from "./LayoutMenu/index.vue";
-import MultiTab from "./MultiTab.vue";
-// import QrCode from "./QrCode.vue";
-import Region from "./Region.vue";
-import SearchBar from "./SearchBar.vue";
-import UploadImage from "./Upload/UploadImage.vue";
-import UploadInput from "./Upload/UploadInput.vue";
+import ActionBar from './ActionBar.vue'
+import ActionButton from './ActionButton.vue'
+import Breadcrumb from './Breadcrumb.vue'
+import Chart from './Chart.vue'
+import Cropper from './Cropper.vue'
+import CropperModal from './CropperModal.vue'
+import Editor from './Editor.vue'
+import FormTable from './FormTable.vue'
+import Filter from './Filter/Filter.vue'
+import FilterItem from './Filter/FilterItem.vue'
+import LayoutHeader from './LayoutHeader.vue'
+import LayoutMenu from './LayoutMenu/index.vue'
+import MultiTab from './MultiTab.vue'
+import QrCode from './QrCode.vue'
+import Region from './Region.vue'
+import SearchBar from './SearchBar.vue'
+import UploadImage from './Upload/UploadImage.vue'
+import UploadInput from './Upload/UploadInput.vue'
 
-import Loading from "./Loading";
-import Preview from "./Preview";
+import Loading from './Loading'
+import Preview from './Preview'
 
-export const loading = Loading;
-export const preview = Preview;
+export const loading = Loading
+export const preview = Preview
 
 const components = [
-  ActionBar,
-  ActionButton,
-  Breadcrumb,
-  Chart,
-  Cropper,
-  CropperModal,
-  Editor,
-  FormTable,
-  Filter,
-  FilterItem,
-  LayoutHeader,
-  LayoutMenu,
-  MultiTab,
-//   QrCode,
-  Region,
-  SearchBar,
-  UploadImage,
-  UploadInput,
-];
+    ActionBar,
+    ActionButton,
+    Breadcrumb,
+    Chart,
+    Cropper,
+    CropperModal,
+    Editor,
+    FormTable,
+    Filter,
+    FilterItem,
+    LayoutHeader,
+    LayoutMenu,
+    MultiTab,
+    QrCode,
+    Region,
+    SearchBar,
+    UploadImage,
+    UploadInput,
+]
 
 export default {
-  install(app) {
-    components.forEach((component) => {
-      app.component(component.name, component);
-    });
-    return app;
-  },
-};
+    install(app) {
+        components.forEach((component) => {
+            app.component(component.name, component)
+        })
+        return app
+    },
+}
