407b9da3afa37ee0bc4097235b79ae989c633b76
mengxianghan <1056811341@qq.com>
2023-03-26 21:38:35 +0800

    fix: 拼写错误

---
+++ src/config/regexp.js
@@ -0,0 +1,1 @@
+export const RE_EMAIL = new RegExp('^((https|http|ftp|rtsp|mms)?:\\/\\/)[^\\s]+', 'g')
--- src/utils/is.js
+++ src/utils/is.js
@@ -1,7 +1,7 @@
-import { REG_EMIAL } from '@/config'
+import { RE_EMAIL } from '@/config/regexp'
 
 /**
  * 验证 url
  * @param {string} str
  */
-export const isUrl = (str) => REG_EMIAL.test(str)
+export const isUrl = (str) => RE_EMAIL.test(str)
