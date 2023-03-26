7187d67597ce781523687e8cd53f2994ac13ca69
mengxianghan <1056811341@qq.com>
2023-03-26 21:39:54 +0800

    feat: 移除未使用的常量

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,1 @@
-export const STORAGE_IS_LOGIN = 'isLogin'
-export const STORAGE_TOKEN = 'token'
-export const STORAGE_USER_INFO = 'userInfo'
-export const STORAGE_PERMISSION = 'permission'
-
 export const URL_LOGO = `${import.meta.env.BASE_URL}images/logo.svg`
-
-export const REG_EMIAL = new RegExp('^((https|http|ftp|rtsp|mms)?:\\/\\/)[^\\s]+', 'g')
