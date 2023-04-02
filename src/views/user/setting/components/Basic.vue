a9c5e06e14271b30c302e645c7e49ed7160e3ea7
mengxianghan <1056811341@qq.com>
2023-04-02 11:43:58 +0800

    feat: 完善个人设置

--- src/views/user/setting/components/Basic.vue
+++ src/views/user/setting/components/Basic.vue
@@ -10,17 +10,23 @@
                 <a-form-item label="昵称">
                     <a-input></a-input>
                 </a-form-item>
-                <a-form-item label="个人介绍">
+                <a-form-item label="个人简介">
+                    <a-textarea></a-textarea>
+                </a-form-item>
+                <a-form-item label="国家/地区">
+                    <a-select></a-select>
+                </a-form-item>
+                <a-form-item label="所在省市">
+                    <x-region></x-region>
+                </a-form-item>
+                <a-form-item label="街道地址">
                     <a-input></a-input>
                 </a-form-item>
-                <a-form-item label="地区">
-                    <a-input></a-input>
-                </a-form-item>
-                <a-form-item label="详细地址">
-                    <a-input></a-input>
-                </a-form-item>
-                <a-form-item label="手机号">
-                    <a-input></a-input>
+                <a-form-item label="联系电话">
+                    <a-space>
+                        <a-input :style="{ width: '72px' }"></a-input>
+                        <a-input :style="{ width: '214px' }"></a-input>
+                    </a-space>
                 </a-form-item>
                 <a-form-item>
                     <a-button type="primary">更新资料</a-button>
--- src/views/user/setting/components/Bind.vue
+++ src/views/user/setting/components/Bind.vue
@@ -1,16 +1,57 @@
 <template>
     <a-list
         item-layout="horizontal"
-        :data-source="list"></a-list>
+        row-key="id"
+        :data-source="list">
+        <template #renderItem="{ item }">
+            <a-list-item>
+                <template #actions>
+                    <a>绑定</a>
+                </template>
+                <a-list-item-meta>
+                    <template #avatar>
+                        <a-avatar
+                            shape="square"
+                            :size="48"
+                            :style="{
+                                backgroundColor: item.color,
+                            }">
+                            <template #icon>
+                                <component :is="item.avatar"></component>
+                            </template>
+                        </a-avatar>
+                    </template>
+                    <template #title>{{ item.title }}</template>
+                    <template #description>{{ item.description }}</template>
+                </a-list-item-meta>
+            </a-list-item>
+        </template>
+    </a-list>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { TaobaoOutlined, AlipayOutlined, DingdingOutlined } from '@ant-design/icons-vue'
 
 export default {
     name: 'Bind',
     setup() {
-        const list = ref([])
+        const list = [
+            { id: '1', title: '绑定淘宝', description: '当前未绑定淘宝账号', avatar: TaobaoOutlined, color: '#ff5800' },
+            {
+                id: '2',
+                title: '绑定支付宝',
+                description: '当前未绑定支付宝账号',
+                avatar: AlipayOutlined,
+                color: '#1677ff',
+            },
+            {
+                id: '3',
+                title: '绑定钉钉',
+                description: '当前未绑定钉钉账号',
+                avatar: DingdingOutlined,
+                color: '#007fff',
+            },
+        ]
 
         return {
             list,
