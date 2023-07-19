e7079bfb5a5e27fc28fe4845043b25e0f593da7d
mengxianghan <1056811341@qq.com>
2023-07-19 21:07:00 +0800

    perf: 接口自动引入

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,10 @@
-import * as common from './modules/common.js'
-import * as system from './modules/system.js'
-import * as user from './modules/user.js'
+const modules = import.meta.glob('./modules/*.js', { eager: true })
 
-const api = {
-    common,
-    system,
-    user,
-}
+const api = {}
+
+Object.keys(modules).forEach((key) => {
+    const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'))
+    api[`${name}Api`] = { ...modules[key] }
+})
 
 export default api
