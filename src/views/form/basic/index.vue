58f1de7cff864143646bf478805c1f9e70625518
mengxianghan <1056811341@qq.com>
2023-08-28 14:59:15 +0800

    fix: 背景色

--- src/views/form/basic/index.vue
+++ src/views/form/basic/index.vue
@@ -1,7 +1,8 @@
 <template>
     <a-page-header
         title="基础表单"
-        main>
+        main
+        :ghost="false">
         表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。
     </a-page-header>
     <a-card class="mt-8-2">
