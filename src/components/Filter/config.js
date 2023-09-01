d4de2714b40778e6d96bc810c39fcf304faa7188
mengxianghan <1056811341@qq.com>
2023-09-01 12:33:25 +0800

    refactor: 移除 Filter 组件

--- src/components/Filter/config.js
+++
@@ -1,7 +0,0 @@
-export const FILTER_KEY = Symbol('filter')
-
-export const FILTER_ITEM_DATA_SOURCE_KEY = Symbol('filterItemDataSource')
-
-export const FILTER_TAG_KEY = Symbol('filterTag')
-
-export const FILTER_TAG_SELECTED_VALUE_KEY = Symbol('filterTagSelectedValue')
