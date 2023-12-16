3be170e5761e76fea615b39bdddc6b1f9f9f6d8a
mengxianghan <1056811341@qq.com>
2023-12-16 21:40:02 +0800

    style: 格式化代码

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import pkg from './package.json'
-
 import useCompressPlugin from './config/useCompressPlugin'
 import useProgressPlugin from './config/useProgressPlugin'
 import useVuePlugin from './config/useVuePlugin'
