a0b64a55c09b9260d69cd976c448c66e784395e9
mengxianghan <1056811341@qq.com>
2024-01-19 11:09:10 +0800

    feat(Transfer): 基础用法 demo

--- src/components/Transfer/demo/Basic.vue
+++ src/components/Transfer/demo/Basic.vue
@@ -8,28 +8,50 @@
         v-model="checkedKeys"
         :data-source="listData"
         :style="{ width: '680px' }"
-        :target-source="targetData"
         show-check-all>
     </x-transfer>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { nanoid } from 'nanoid'
 
-const listData = ref(getList())
-const targetData = ref()
+const listData = ref([
+    {
+        label: 'content 1',
+        value: '1',
+        children: [
+            { label: 'content 1-1', value: '1-1' },
+            { label: 'content 1-2', value: '1-2' },
+        ],
+    },
+    {
+        label: 'content 2',
+        value: '2',
+        children: [
+            { label: 'content 2-1', value: '2-1' },
+            { label: 'content 2-2', value: '2-2' },
+        ],
+    },
+    {
+        label: 'content 3',
+        value: '3',
+        disabled: true,
+        children: [
+            { label: 'content 3-1', value: '3-1' },
+            { label: 'content 3-2', value: '3-2' },
+        ],
+    },
+    {
+        label: 'content 4',
+        value: '4',
+        checkable: false,
+        children: [
+            { label: 'content 4-1', value: '4-1' },
+            { label: 'content 4-2', value: '4-2' },
+        ],
+    },
+])
 const checkedKeys = ref([])
-
-function getList(length = 10) {
-    return [...Array(length)].map(() => {
-        const value = nanoid(5)
-        return {
-            label: value,
-            value,
-        }
-    })
-}
 </script>
 
 <style lang="less" scoped></style>
