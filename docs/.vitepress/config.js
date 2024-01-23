236137f6664920854df9b8003c0c735b27a01f33
mengxianghan <1056811341@qq.com>
2024-01-23 21:24:37 +0800

    feat: 修改文档配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vitepress'
-import path from 'path'
 import nav from './nav'
 import sidebar from './sidebar'
 
@@ -7,7 +6,7 @@ import sidebar from './sidebar'
 export default defineConfig({
     title: 'XYAdmin',
     description: 'xy admin 使用手册',
-    appearance: false,
+    appearance: true,
     themeConfig: {
         logo: 'http://cdn.xuanyunet.com/images/logo.svg',
         outline: 'deep',
@@ -18,52 +17,14 @@ export default defineConfig({
         footer: {
             message: '本文档内容版权为 XYAdmin 作者所有，保留所有权利。',
         },
-        // algolia: {
-        //     appId: 'U22360281K',
-        //     apiKey: '3e6f9ead870588058fc7d07ae6a344fa',
-        //     indexName: 'xy-admin-xuanyunet',
-        // },
     },
     markdown: {
         lineNumbers: true,
     },
-    head: [
-        [
-            'script',
-            { type: 'application/javascript', src: 'http://xy-admin.xuanyunet.com/libs/tinymce/tinymce.min.js' },
-        ],
-    ],
     vite: {
         server: {
             host: true,
-            port: 9212,
-        },
-        build: {
-            target: 'es2015',
-            cssTarget: 'chrome80',
-            brotliSize: false,
-            chunkSizeWarningLimit: 2000,
-        },
-        css: {
-            preprocessorOptions: {
-                less: {
-                    modifyVars: {
-                        hack: `
-                            true;
-                            @import '${path.resolve(__dirname, '../../src/styles/mixins/index.less')}';
-                            @import '${path.resolve(__dirname, '../../src/styles/variables.less')}';
-                            @import '${path.resolve(__dirname, '../../src/styles/utils.less')}';
-                         `,
-                    },
-                    javascriptEnabled: true,
-                },
-            },
-            devSourcemap: true,
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '../../src'),
-            },
+            port: 8000,
         },
     },
 })
