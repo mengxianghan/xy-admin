1d47152ac071d416a11ffd9d073a4b7e599f3005
mengxianghan <1056811341@qq.com>
2024-01-25 17:20:14 +0800

    feat(Toolbar): 新增 bordered 属性

--- src/components/Toolbar/Toolbar.vue
+++ src/components/Toolbar/Toolbar.vue
@@ -1,24 +1,46 @@
 <template>
-    <a-row class="x-toolbar">
-        <a-col flex="1">
-            <slot></slot>
-        </a-col>
-        <a-col v-if="cpShowExtra">
-            <slot name="extra"></slot>
-        </a-col>
-    </a-row>
+    <div class="x-toolbar">
+        <define-template>
+            <a-row class="x-toolbar">
+                <a-col flex="1">
+                    <slot></slot>
+                </a-col>
+                <a-col v-if="cpShowExtra">
+                    <slot name="extra"></slot>
+                </a-col>
+            </a-row>
+        </define-template>
+
+        <template v-if="bordered">
+            <a-card>
+                <reuse-template></reuse-template>
+            </a-card>
+        </template>
+        <template v-else>
+            <reuse-template></reuse-template>
+        </template>
+    </div>
 </template>
 
 <script setup>
 import { computed, useSlots } from 'vue'
+import { createReusableTemplate } from '@vueuse/core'
 
 defineOptions({
     name: 'XToolbar',
 })
 
+defineProps({
+    bordered: {
+        type: Boolean,
+        default: false,
+    },
+})
+
 const slots = useSlots()
 
 const cpShowExtra = computed(() => !!slots.extra)
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate()
 </script>
 
 <style lang="less" scoped></style>
---
+++ src/components/Toolbar/demo/Bordered.vue
@@ -0,0 +1,27 @@
+<demo>
+## 带边框
+圈（juàn）起来，更聚焦
+</demo>
+
+<!-- #region snippet -->
+<template>
+    <x-toolbar bordered>
+        <a-button type="primary">新建数据</a-button>
+
+        <template #extra>
+            <a-space>
+                <a-input></a-input>
+                <a-button
+                    ghost
+                    type="primary">
+                    查询
+                </a-button>
+            </a-space>
+        </template>
+    </x-toolbar>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
+<!-- #endregion snippet -->
