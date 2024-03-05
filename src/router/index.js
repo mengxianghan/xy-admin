7d352fb67b42259ead0a5c45b4e3371f6156db05
mengxianghan <1056811341@qq.com>
2024-03-05 11:48:57 +0800

    feat: 新增 open、reLaunch 路由方法

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,26 @@ const router = createRouter({
     routes: [...constantRoutes],
 })
 
+/**
+ * 打开新标签页
+ * @param to
+ */
+router.open = function (to) {
+    const { href } = router.resolve(to)
+    if (!href) return
+    window.open(href)
+}
+
+/**
+ * 重新启动
+ * @param to
+ */
+router.reLaunch = function (to) {
+    const { href } = router.resolve(to)
+    if (!href) return
+    window.location.href = href
+}
+
 export const setupRouter = (app) => {
     app.use(router)
     return app
