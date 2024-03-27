c1a62b7a44037d56d8c15479c4ef34e1bc811464
mengxianghan <1056811341@qq.com>
2024-03-27 09:21:19 +0800

    feat(TableColumnSetting): 新增动态列 demo

--- src/components/Table/demo/index.vue
+++ src/components/Table/demo/index.vue
@@ -1,10 +1,11 @@
 <template>
-    <x-demo-box-group :components="[Basic, Custom]"></x-demo-box-group>
+    <x-demo-box-group :components="[Basic, Custom, DynamicColumns]"></x-demo-box-group>
 </template>
 
 <script setup>
 import Basic from './Basic.vue'
 import Custom from './Custom.vue'
+import DynamicColumns from './DynamicColumns.vue'
 </script>
 
 <style lang="less" scoped></style>
