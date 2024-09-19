0ffd7ee17a30304b3e8f086d999ac35fd383f0ac
MengXianghan <1056811341@qq.com>
2024-09-19 11:09:05 +0800

    docs: 导出 vitepress 的 Badge 组件

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,9 +1,11 @@
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme, { VPBadge } from 'vitepress/theme'
 import Layout from './Layout.vue'
 import './index.css'
 
 export default {
     ...DefaultTheme,
     Layout,
-    enhanceApp({ app }) {},
+    enhanceApp({ app }) {
+        app.component('Badge', VPBadge)
+    },
 }
